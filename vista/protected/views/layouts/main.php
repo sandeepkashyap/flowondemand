02d06fa339f63e6ec4cb987de2423b1058fed1a0
donga.spirit <donga.spirit@435a746b-1b3c-0410-9f04-db2354e518cf>
2009-11-04 17:10:52 +0000

    fix some tickets

--- vista/protected/views/layouts/main.php
+++ vista/protected/views/layouts/main.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/2000/REC-xhtml1-200000126/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="language" content="en" />
 <?php
 /*echo CHtml::cssFile(Yii::app()->request->baseUrl.'/css/reset.css');
