f71f8928bec701af1856e3e1bb8536ab0b816535
phuongmathy <phuongmathy@435a746b-1b3c-0410-9f04-db2354e518cf>
2009-11-16 16:52:47 +0000

    Sort apps by vs_name asc

--- trunk/vista/protected/modules/apps/views/application/index.php
+++ trunk/vista/protected/modules/apps/views/application/index.php
@@ -5,8 +5,8 @@
 //	$models = application::model()->findAllBySql("SELECT a.* FROM apps a JOIN app_user_admin t ON a.id = t.app_id AND t.user_id = " . Yii::app()->user->id);
 //	$models = application::model()->findAllBySql("SELECT a.* FROM apps a WHERE a.id_client = " . Yii::app()->user->id . " ORDER BY vc_name");
 	$arr_apps = CHtml::listData($models, 'id', 'vc_name');
-	$arr_apps[0] = '';
-	ksort($arr_apps);
+	//$arr_apps[0] = '';
+	//ksort($arr_apps);
 	echo CHtml::dropDownList('id_application', '', $arr_apps, array('style'=>'width: 200px; text-align: center', 'id'=>'id_application')); 
 ?>
 <script language="JavaScript" type="text/javascript">
